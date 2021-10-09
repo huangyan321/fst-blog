79732a576f7cb724fc13f17688b1f23eef7cd3e9
huangyan321 <1604549268@qq.com>
2021-10-09 09:32:07 +0800

    add jwt globalkey

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+global.globalkey = "123456" //全局key
 const Utils = require('./utils');
 const Tips = require('./utils/tip');
 const router = require('./routes');
@@ -11,7 +11,9 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
