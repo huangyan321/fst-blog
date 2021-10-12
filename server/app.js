27f3c8b0914bcd51a61d82d51586a082a597c161
huangyan321 <1604549268@qq.com>
2021-10-12 23:55:08 +0800

    允许跨域

--- server/app.js
+++ server/app.js
@@ -3,13 +3,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 global.globalkey = "123456" //全局key
-const Utils = require('./utils');
-const Tips = require('./utils/tip');
-const router = require('./routes');
+
 
 var app = express();
 
+//跨域配置
+app.all("*", function (req, res, next) {
+	//设置允许跨域的域名，*代表允许任意域名跨域
 
+	res.header("Access-Control-Allow-Origin", "*");
+	//允许的header类型
+	res.header("Access-Control-Allow-Headers", "Content-Type,Access-Token,Appid,Secret,Authorization");
+	//跨域允许的请求方式 
+	res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+	if (req.method.toLowerCase() == 'options')
+		res.sendStatus(200); //让options尝试请求快速结束
+	else
+		next();
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
@@ -17,6 +28,6 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(__dirname + '/static'))
 require('./routes/admin/index')(app)
-router(app);
 module.exports = app;
