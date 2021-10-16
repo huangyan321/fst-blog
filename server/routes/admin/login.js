c674bb193afad4fe6464bd770ed4e6bf28216e41
huangyan321 <1604549268@qq.com>
2021-10-17 01:08:42 +0800

    登录接口更换

--- server/routes/admin/login.js
+++ server/routes/admin/login.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const user = require("../../dao/User");
+const user = require("../../dao/Admin");
 /* GET users listing. */
 router.post("/login", function (req, res, next) {
   user.login(req, res);
