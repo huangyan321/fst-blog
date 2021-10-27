ce68f3ffc5f022b7924c3ece344208418dc850de
huangyan321 <1604549268@qq.com>
2021-10-27 16:58:46 +0800

    topbar 组件完善样式 加入router-link可返回主页

---
+++ front/src/components/common/Top.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <header class="top-header">
+      <img
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAQAAAD9CzEMAAAAIGNIUk0AAHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAAACYktHRAD/h4/MvwAAAAlwSFlzAAALEwAACxMBAJqcGAAAADlJREFUWMPt08ERACAIBDEomvqxBh18EQogn9us+HsJAAAAt0BPfQU8A2YKULKSdQAQmpIBAMBm4AAtdCIhmRB8RAAAAABJRU5ErkJggg=="
+        alt=""
+        class="top-header__menu-button"
+        @click="toggleSidebox"
+      />
+
+      <router-link to="/" class="top-header__main-icon">blog</router-link>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+  methods: {
+    toggleSidebox() {
+      this.$store.commit('side/TOGGLE_SIDE_BOX')
+    }
+  },
+};
+</script>
+<style lang="stylus" scoped>
+@import '../../assets/stylus/_settings.styl';
+
+.top-header {
+  position: fixed;
+  top: 0;
+  height: 60px;
+  line-height: @height;
+  width: 100%;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.25);
+  padding: 0 40px;
+  background: white;
+  z-index: 3;
+
+  &__main-icon {
+    text-decoration: none;
+    color: $blue;
+    font-weight: 600;
+    font-size: 20px;
+  }
+
+  &__menu-button {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    width: 32px;
+    height: 32px;
+    margin-top: -(@height / 2);
+    display: none;
+  }
+}
+
+// 屏幕宽度小于850px时 字体居中
+@media screen and (max-width: 850px) {
+  .top-header {
+    text-align: center;
+    padding: 0;
+
+    &__menu-button {
+      display: block;
+    }
+  }
+}
+</style>
