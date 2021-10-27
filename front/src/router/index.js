667a299f9128b3d7cfe60fb269c03628de18ac8e
huangyan321 <1604549268@qq.com>
2021-10-27 16:59:11 +0800

    添加路由

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import List from '../views/list.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',component: () => import('../views/list.vue')
+  }
 ]
 
 const router = new VueRouter({
