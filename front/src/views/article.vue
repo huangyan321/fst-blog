9472c5ebfdd3affd61199cfe5c6cb5e4cf30ad1b
huangyan321 <1604549268@qq.com>
2021-11-02 17:15:55 +0800

    初始化文章模块

---
+++ front/src/views/article.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>文章页</div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped></style>
