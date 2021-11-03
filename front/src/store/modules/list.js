6ac8c96079eb986a5eaa85ff2b3aa3d1a13d99c1
huangyan321 <1604549268@qq.com>
2021-11-03 15:13:29 +0800

    add article api

--- front/src/store/modules/list.js
+++ front/src/store/modules/list.js
@@ -1,10 +1,10 @@
-import { queryAllBlogs } from '../../api/article'
-
+import { queryAllBlogs, queryOneBlog } from '../../api/article'
+import markdownCompile from '../../utils/marked'
 const state = {
   blogs: [],
   pageNum: 0,
   pageSize: 0,
-  currentBlogs: {
+  currentBlog: {
     id: '',
     content: ''
   },
@@ -13,6 +13,10 @@ const state = {
 const mutations = {
   GET_ALL_BLOGS(state, blogs) {
     state.blogs = blogs
+  },
+  GET_ONE_BLOGS(state, blog) {
+    state.currentBlog = blog
+    state.currentlyCompiledBlog = markdownCompile(blog.content)
   }
 }
 const actions = {
@@ -24,6 +28,14 @@ const actions = {
           resolve(res)
         })
       }
+    })
+  },
+  getOneBlog({ commit, state }, blog_id) {
+    return queryOneBlog(blog_id).then(res => {
+      commit('GET_ONE_BLOGS', res.data)
+      return new Promise((resolve, reject) => {
+        resolve(res)
+      })
     })
   }
 }
