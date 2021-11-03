da48a08b6ad41894b6c80fc21c80fae408ce016e
huangyan321 <1604549268@qq.com>
2021-11-03 12:34:43 +0800

    添加博客获取逻辑

---
+++ front/src/store/modules/list.js
@@ -0,0 +1,36 @@
+import { queryAllBlogs } from '../../api/article'
+
+const state = {
+  blogs: [],
+  pageNum: 0,
+  pageSize: 0,
+  currentBlogs: {
+    id: '',
+    content: ''
+  },
+  currentlyCompiledBlog: ''
+}
+const mutations = {
+  GET_ALL_BLOGS(state, blogs) {
+    state.blogs = blogs
+  }
+}
+const actions = {
+  getAllBlogs({ commit, state }, listQuery) {
+    return queryAllBlogs(listQuery).then(res => {
+      if (res.code === 200) {
+        commit('GET_ALL_BLOGS', res.data)
+        return new Promise((resolve, reject) => {
+          resolve(res)
+        })
+      }
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
--- front/src/views/list.vue
+++ front/src/views/list.vue
@@ -5,30 +5,39 @@
       <Loading :text="loadingMsg"></Loading>
     </div>
     <ul class="list__article">
-      <li class="list__article__filter">
-        筛选
-        <span>vue</span>
-        分类
-      </li>
-      <template>
-        <li class="list__article__item" v-for="item in 3">
-          <h1 class="list__article__item__title">标题</h1>
+      <transition name="slide-fade">
+        <li class="list__article__filter" v-if="selectTags.length !== 0">
+          筛选
+          <span>{{ filterMsg }}</span>
+          分类
+        </li>
+      </transition>
+      <template v-if="blogs.length !== 0 && showLoading == false">
+        <li class="list__article__item" v-for="blog in blogs" :key="blog.id">
+          <h1 class="list__article__item__title">{{ blog.title }}</h1>
           <div class="list__article__item__info">
-            <p class="list__article__item__time">时间</p>
-            <div class="list__article__item__abstract markdown-body">内容</div>
+            <p class="list__article__item__time">{{ blog.update_time }}</p>
+            <div
+              class="list__article__item__abstract markdown-body"
+              v-html="compiledMarkdown(blog.brief)"
+            >
+              {{ blog.brief }}
+            </div>
             <p>
               <router-link to="" class="continue-reading">继续阅读...</router-link>
             </p>
           </div>
         </li>
       </template>
-      <div class="msg-box">暂时没有相关内容</div>
+      <div class="msg-box" v-if="blogs.length === 0">暂时没有相关内容</div>
     </ul>
   </div>
 </template>
 <script>
 import Side from '../components/common/Side'
 import Loading from '../components/Loading'
+import { mapGetters, mapActions } from 'vuex'
+import mark from '../utils/marked'
 export default {
   components: {
     Side,
@@ -36,15 +45,58 @@ export default {
   },
   data() {
     return {
+      listQuery: {
+        pageNum: 1,
+        pageSize: 5,
+        type: 0
+      },
       loadingMsg: '疯狂加载中！奥里给干了！！',
       showLoading: false
     }
   },
+  computed: {
+    ...mapGetters(['selectTags', 'blogs']),
+    filterMsg() {
+      let msg = ''
+      this.selectTags.forEach((item, index) => {
+        msg += item.name + '、'
+      })
+      return msg.replace(/、$/, '')
+    }
+  },
+  beforeMount() {
+    let that = this
+    this.showLoading = true
+    let res = this.getAllBlogs(this.listQuery)
+    setTimeout(() => {
+      that.showLoading = false
+    }, 5000)
+  },
   mounted() {},
-  methods: {}
+  methods: {
+    compiledMarkdown(value) {
+      return mark(value)
+    },
+    ...mapActions('list', {
+      getAllBlogs: 'getAllBlogs'
+    })
+  }
 }
 </script>
 <style lang="stylus" scoped>
+/* 可以设置不同的进入和离开动画 */
+/* 设置持续时间和动画函数 */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 .list
   padding 10px
   max-width 1000px
