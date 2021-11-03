0544f19077ffa195d3074de2fce222da71643115
huangyan321 <1604549268@qq.com>
2021-11-03 17:09:24 +0800

    store 全局控制切页

--- front/src/store/modules/list.js
+++ front/src/store/modules/list.js
@@ -2,8 +2,10 @@ import { queryAllBlogs, queryOneBlog } from '../../api/article'
 import markdownCompile from '../../utils/marked'
 const state = {
   blogs: [],
-  pageNum: 0,
-  pageSize: 0,
+  pageNum: 1,
+  pageSize: 4,
+  type: 0,
+  total: 0,
   currentBlog: {
     id: '',
     content: ''
@@ -11,19 +13,28 @@ const state = {
   currentlyCompiledBlog: ''
 }
 const mutations = {
-  GET_ALL_BLOGS(state, blogs) {
-    state.blogs = blogs
+  GET_ALL_BLOGS(state, res) {
+    state.blogs = res.data
+    state.total = res.total
   },
   GET_ONE_BLOGS(state, blog) {
     state.currentBlog = blog
     state.currentlyCompiledBlog = markdownCompile(blog.content)
+  },
+  TRIGGER_PAGE(state, page) {
+    page.pageNum ? (state.pageNum = page.pageNum) : ''
+    page.pageSize ? (state.pageSize = page.pageSize) : ''
   }
 }
 const actions = {
-  getAllBlogs({ commit, state }, listQuery) {
-    return queryAllBlogs(listQuery).then(res => {
+  getAllBlogs({ commit, state }) {
+    return queryAllBlogs({
+      pageNum: state.pageNum,
+      pageSize: state.pageSize,
+      type: state.type
+    }).then(res => {
       if (res.code === 200) {
-        commit('GET_ALL_BLOGS', res.data)
+        commit('GET_ALL_BLOGS', res)
         return new Promise((resolve, reject) => {
           resolve(res)
         })
