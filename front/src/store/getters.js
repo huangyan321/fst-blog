cbbbed429a4100173576bd4df2de95247141edb7
huangyan321 <1604549268@qq.com>
2021-11-03 12:33:19 +0800

    add getters

--- front/src/store/getters.js
+++ front/src/store/getters.js
@@ -1,5 +1,11 @@
 const getters = {
   sideBoxOpen: state => state.side.sideBoxOpen,
-  tags: state => state.side.tags
+  tags: state => state.side.tags,
+  selectTags: state => state.side.selectTags,
+  blogs: state => state.list.blogs,
+  pageNum: state => state.list.pageNum,
+  pageSize: state => state.list.pageSize,
+  currentlyCompiledBlog: state => state.list.currentlyCompiledBlog,
+  currentBlogs: state => state.list.currentBlogs
 }
 export default getters
