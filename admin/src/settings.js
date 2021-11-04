36032b4c827b5084ad501142fde7426704ecff22
huangyan321 <1604549268@qq.com>
2021-11-04 19:33:12 +0800

    fix title

--- admin/src/settings.js
+++ admin/src/settings.js
@@ -1,6 +1,5 @@
 module.exports = {
-
-  title: 'Vue Admin Template',
+  title: "blog-admin-platform",
 
   /**
    * @type {boolean} true | false
@@ -13,4 +12,4 @@ module.exports = {
    * @description Whether show the logo in sidebar
    */
   sidebarLogo: false
-}
+};
